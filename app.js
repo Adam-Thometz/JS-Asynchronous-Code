05f243ee7164e6d9c563591be1ec8ef13747cbdc
Adam Thometz <adamthometz@gmail.com>
2021-08-20 16:24:16 -0400

    version with async/await

--- app.js
+++ app.js
@@ -8,28 +8,28 @@ const submitBtn = document.getElementById('submitBtn')
 const numsList = document.getElementById('numsList')
 const errorList = document.getElementById('errorList')
 
-submitBtn.addEventListener('click', getNumberInfo)
+submitBtn.addEventListener('click', getNumberInfoAsync)
 
-function getNumberInfo() {
+async function getNumberInfoAsync() {
   numsList.innerHTML = ''
   let nums = document.getElementById('numbers').value.split(',')
+  if (nums[0] === "") {
+    errorList.innerText = 'Input cannot be empty!'
+    return
+  };
   nums.forEach(n => parseInt(n))
-  const numRes = []
-  for (let i = 0; i < nums.length; i++) {
-    numRes.push(
-      axios.get(`${url1}/${parseInt(nums[i])}`)
-    )
+  for (let num of nums) {
+    try {
+      const res = await axios.get(`${url1}/${num}`)
+      const numFact = document.createElement('li')
+      numFact.innerText = res.data
+      numsList.append(numFact)
+    } catch (e) {
+      errorList.innerText = 'There was an error. Try again'
+      return
+    }
+  errorList.innerText = ''
   }
-
-  Promise.all(numRes)
-    .then(numRes => {
-      for (let res of numRes) {
-        const numFact = document.createElement('li')
-        numFact.innerText = res.data
-        numsList.append(numFact)
-      }
-    })
-    .catch((err) => console.log(err));
 }
 
 /////////////////////////////////
@@ -38,45 +38,56 @@ function getNumberInfo() {
 
 const url2 = 'http://deckofcardsapi.com/api';
 const apiNewDeck = 'deck/new/shuffle';
-const apiDrawCard = (deckId) => `deck/${deckId}/draw`;
 
 const deckStatus = document.getElementById('deckStatus');
 const shuffleDeck = document.getElementById('shuffleDeck');
 const drawCard = document.getElementById('drawCard');
 const cardHolder = document.getElementById('cardHolder')
 
-shuffleDeck.addEventListener('click', shuffle);
-drawCard.addEventListener('click', draw);
+class Deck {
+  constructor() {
+    this.id,
+    this.remaining,
+    this.currCard
+  }
 
-let deckId = null
-
-function shuffle() {
-  res = axios.get(`${url2}/${apiNewDeck}`)
-    .then(res => {
-      deckId = res.data.deck_id;
-      cardHolder.innerHTML = ''
+  async shuffle() {
+    try {
+      const res = await axios.get(`${url2}/${apiNewDeck}`);
+      this.id = res.data.deck_id;
+      this.remaining = res.data.remaining;
+      cardHolder.innerHTML = '';
       deckStatus.innerText = "You have a fresh deck! Start drawing";
-    })
-    .catch(err => console.log(err));
-}
+    } catch (e) {
+      deckStatus.innerText = 'Whoops, something went wrong! Try again later';
+    }
+  }
 
-function draw() {
-  res = axios.get(`${url2}/${apiDrawCard(deckId)}`)
-    .then(res => {
-      card = res.data.cards[0];
-      remaining = res.data.remaining
+  async draw() {
+    try {
+      const res = await axios.get(`${url2}/deck/${this.id}/draw/?count=1`);
+      this.currCard = res.data.cards[0];
+      this.remaining = res.data.remaining;
       const cardImg = document.createElement('img');
-      cardImg.src = card.image;
-      cardImg.alt = `An image of the ${card.value} of ${card.suit} card`;
+      cardImg.src = this.currCard.image;
+      cardImg.alt = `An image of the ${this.currCard.value} of ${this.currCard.suit} card`;
 
       cardHolder.prepend(cardImg)
 
-      deckStatus.innerText = `You drew a ${card.value} of ${card.suit}! There are ${remaining} cards left in this deck.`;
+      deckStatus.innerText = `You drew a ${this.currCard.value} of ${this.currCard.suit}! There are ${this.remaining} cards left in this deck.`;
       if (remaining === 0) {
         drawCard.style.display = 'none'
       }
-    })
-    .catch(() => {
-      deckStatus.innerText = 'Whoops, something went wrong! Try again or reshuffle the deck!'
-    })
+    } catch (e) {
+      deckStatus.innerText = 'Whoops, something went wrong! Try again or reshuffle the deck!';
+    }
   }
+}
+
+let deck = new Deck()
+shuffleDeck.addEventListener('click', function() {
+  deck.shuffle()
+})
+drawCard.addEventListener('click', function() {
+  deck.draw()
+})
